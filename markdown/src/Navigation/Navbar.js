bd20cb54097156d8273a491e972892cf9dbab027
emarahch <emarah.charles19@bcmail.cuny.edu>
2023-07-07 19:27:07 -0400

    :(

--- markdown/src/Navigation/Navbar.js
+++ markdown/src/Navigation/Navbar.js
@@ -1,47 +1,90 @@
 // https://www.codevertiser.com/reactjs-responsive-navbar/
 
-import { useState, Button } from 'react'
+import { useState, Button } from "react";
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import DarkModeToggle from './darkModeToggle';
-import addNoteButton from './addNote';
-import './Navbar.css';
-
-
-
+import { NavLink } from "react-router-dom";
+import DarkModeToggle from "./darkModeToggle";
+// import addNoteButton from './addNote';
+import "./Navbar.css";
 
 const Navbar = () => {
-    const [showNavbar, setShowNavbar] = useState(false)
+  const [showNavbar, setShowNavbar] = useState(false);
+  const [addNotelist, setNoteList] = useState(0);
+  const notes = [];
 
-    const handleShowNavbar = () => {
-      setShowNavbar(!showNavbar)
-    }
+  // const handleShowNavbar = () => {
+  //   setShowNavbar(!showNavbar);
+  // };
+
+
+  const ParentComponent = ({ notes, incrementCount }) => {
     return (
-        <nav className="navbar">
-        <div className="container">
-          <div className="logo">
-          </div>
-          <div className="nav-elements">
-            <ul>
+      <>
+        <button onClick={incrementCount}>Add another component</button>
+        <div>{notes}</div>
+        
+      </>
+    );
+  };
+  
+  const ChildComponent = () => {
+    return <h4>This is a child component</h4>;
+  };
+
+
+
+  
+
+  // Function to increment count by 1
+  const incrementCount = () => {
+    setNoteList(addNotelist+1);
+   
+  };
+
+
+
+
+
+
+
+
+
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo"></div>
+        <div className="nav-elements">
+          <ul>
             <li>
               <button type="button">Back</button>
-              </li>
-              <li>
-              <DarkModeToggle/>
-              </li>
-              <li>
-                <NavLink to="/">Sound</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-              <li>
-              <addNoteButton/>
-              </li>
-            </ul>
-          </div>
+            </li>
+            <li>
+              <DarkModeToggle />
+            </li>
+            <li>
+              <NavLink to="/">Sound</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <div className="Notes">
+                {/* {addNotelist} */}
+                
+                <ParentComponent addComponent={incrementCount}></ParentComponent>
+                
+                {/* <button onClick={incrementCount} type="button">
+                  Add note
+                </button> */}
+              </div>
+            </li>
+          </ul>
         </div>
-      </nav>
-      )
-}
-export default Navbar
+      </div>
+    </nav>
+  );
+};
+
+
+export default Navbar;
--- markdown/src/Navigation/addNote.js
+++ markdown/src/Navigation/addNote.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Button} from 'react';
 
-
-export const addNoteButton = () => {
+ const addNoteButton = () => {
 
     return (
-        <button type="button">Add note</button>
-    )
+            //   <button type="button">Add note</button>
+            <img
+            src="https://i.imgur.com/MK3eW3As.jpg"
+            alt="Katherine Johnson"
+          />
+        
+        );
   }
   
   export default addNoteButton;
